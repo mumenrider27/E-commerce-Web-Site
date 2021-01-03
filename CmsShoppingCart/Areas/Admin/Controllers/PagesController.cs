bc520f6050527457fedc68415bfdbecb2f286bd7
afrke92505@gmail.com <afrke92505@gmail.com>
2021-01-03 20:50:09 +0400

    Admin Post Create Page Validation

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
@@ -17,6 +18,8 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         {
             this.context = context;
         }
+
+        // GET /admin/pages
         public async Task<IActionResult> Index()
         {
             IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
@@ -24,6 +27,44 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             List<Page> pagesList = await pages.ToListAsync();
 
             return View(pagesList);
+        }        
+        
+        // GET /admin/pages/details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // GET /admin/pages/create
+        public IActionResult Create() => View();
+
+        // POST /admin/pages/create
+        [HttpPost]
+        public async Task<IActionResult> Create(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
+
+                var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The title already exists.");
+                    return View(page);
+                }
+                context.Add(page);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return View(page);
         }
     }
 }
