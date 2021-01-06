0050764c6c63efcb52b2ef45f561684ce9ed39aa
afrke92505@gmail.com <afrke92505@gmail.com>
2021-01-07 01:57:50 +0400

    Admin son düzlük (completely arbitrary language which means "Admin last stage")

---
+++ CmsShoppingCart/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
